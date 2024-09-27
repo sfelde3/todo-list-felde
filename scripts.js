39351fec8c77163b7d1c2391966dd202b8bb6fef
Sergej <sfelde3@gmail.com>
2024-09-27 01:01:44 +0000

    alle Dateien sind erzeugt und befühlt

---
+++ scripts.js
@@ -0,0 +1,36 @@
+function loadTodos(){
+    fetch('http://sf.mshome.net/todo-list-felde/todo.php')
+    .then(response => response.json())
+    .then(todos => {
+        const todoList = 
+        document.getElementById('todoList');
+        todoList.innerHTML = '' ;
+        todos.forEach(todo => {
+            const li = 
+                document.createElement('li');
+                li.textContent = todo;
+                todoList.appendChild(li);
+        });
+    })
+    .catch(error => console.error('Fehler:', error));
+}
+
+    document.getElementById('todoForm').addEventListener('submit' , (evt) =>  {
+        evt.preventDefault();
+        const todoInput = document.getElementById('todoInput').value;
+        fetch('http://sf.mshome.net/todo-list-felde/todo.php', {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json',
+            }, 
+            body: JSON.stringify({todo: todoInput}),
+        })
+        .then(response => response.json())
+        .then((data) => {console.log(data);
+            loadTodos();
+            document.getElementById('todoInput').value = '';
+        })
+        .catch(error => console.error('Fehler:', error));
+    });
+
+window.onload = (event) => loadTodos();
---
+++ todo.php
@@ -0,0 +1,39 @@
+<?php
+   header('Content-Type: application/json');
+
+   //log-funktion
+
+   function write_log($action, $data){
+      $log = '';
+      if(file_exists('log.txt')){
+         $log = fopen('log.txt', 'a');
+      }
+      else{
+         echo "Log-Datei nicht vorhanden";
+         $log = fopen('log.txt', 'w');
+      }
+     
+      $timestamp = date('Y-m-d H:i:s');
+      fwrite($log, "$timestamp - $action: " . json_encode($data) . "\n");
+      fclose($log);
+   }
+   $file = 'todo.json';
+   if(file_exists($file)){
+    $json_data = file_get_contents($file);
+    $todos = json_decode($json_data, true);
+    write_log("read todo json", $todos);
+   }else{
+    $todos = [];
+   }
+   
+
+   if($_SERVER['REQUEST_METHOD'] === 'POST'){
+      $input = json_decode(file_get_contents('php://input'), true);
+      $todos[] = $input['todo'];
+      file_put_contents($file, json_encode($todos));
+      echo json_encode(['status' => 'success']);
+      exit;
+   }
+   echo json_encode($todos);
+
+?>
